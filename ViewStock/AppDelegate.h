34612938d77878f9f5856f554a353dc059ccc079
NGE <nge@dingfangyuandemacbook-pro.local>
2019-01-22 10:31:54 +0800

    Initial Commit

---
+++ ViewStock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ViewStock
+//
+//  Created by NGE on 2019/1/22.
+//  Copyright © 2019年 ViewStock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
